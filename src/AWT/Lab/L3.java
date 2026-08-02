8e204803e971cffcb39859891a8d8f48f153c96d
S i r H u s k y <bovonnatchaladdee@gmail.com>
2026-08-03 02:37:28 +0700

    LAB 03

--- src/AWT/Lab/L3.java
+++ src/AWT/Lab/L3.java
@@ -11,22 +11,22 @@ public class L3 extends Frame {
 
         for(int i=1; i<=50; i++){
             Random rnd = new Random();
-            int x = rnd.nextInt(1850);
-            int y = rnd.nextInt(1000);
+            int x = rnd.nextInt(1820);
+            int y = rnd.nextInt(980);
 
             Button btn = new Button("Button " + i);
             btn.setBounds(x, y, 100, 50);
-            btn.setBackground(randomColor());
+            btn.setBackground(randomColor(rnd));
             add(btn);
         }
 
         setVisible(true);
     }
 
-    public Color randomColor() {
-        int r = (int)(Math.random() * 256);
-        int g = (int)(Math.random() * 256);
-        int b = (int)(Math.random() * 256);
+    public Color randomColor(Random rnd) {
+        int r = rnd.nextInt(256);
+        int g = rnd.nextInt(256);
+        int b = rnd.nextInt(256);
         return new Color(r, g, b);
     }
     public static void main(String[] args) {
